1905385303b1905cc4f99ab2b10e0f2a10000116
Sayantan Patra <68266225+sayantancodex@users.noreply.github.com>
2024-08-04 22:08:55 +0530

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,88 +1,238 @@
-(async () => {
-  await loadPolygonPath(tsParticles);
-
-  await tsParticles.load({
+// https://codepen.io/matteobruni/pen/rNmMVEx
+tsParticles.load("tsparticles", {
+  fullScreen: {
+    enable: true,
+  },
+  fpsLimit: 60,
+  particles: {
+    groups: {
+      z5000: {
+        number: {
+          value: 70,
+        },
+        zIndex: {
+          value: 5000,
+        },
+      },
+      z7500: {
+        number: {
+          value: 30,
+        },
+        zIndex: {
+          value: 75,
+        },
+      },
+      z2500: {
+        number: {
+          value: 50,
+        },
+        zIndex: {
+          value: 25,
+        },
+      },
+      z1000: {
+        number: {
+          value: 40,
+        },
+        zIndex: {
+          value: 10,
+        },
+      },
+    },
+    number: {
+      value: 200,
+      density: {
+        enable: false,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#fff",
+      animation: {
+        enable: false,
+        speed: 20,
+        sync: true,
+      },
+    },
+    shape: {
+      type: "circle",
+    },
+    opacity: {
+      value: 1,
+      random: false,
+      animation: {
+        enable: false,
+        speed: 3,
+        minimumValue: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+    },
+    links: {
+      enable: false,
+      distance: 100,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      angle: {
+        value: 10,
+        offset: 0,
+      },
+      enable: true,
+      speed: 5,
+      direction: "right",
+      random: false,
+      straight: true,
+      outModes: {
+        default: "out",
+      },
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+    zIndex: {
+      value: 5,
+      opacityRate: 0.5,
+    },
+  },
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onHover: {
+        enable: false,
+        mode: "repulse",
+      },
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        links: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 0.8,
+      },
+      repulse: {
+        distance: 200,
+      },
+      push: {
+        quantity: 4,
+        groups: ["z5000", "z7500", "z2500", "z1000"],
+      },
+      remove: {
+        quantity: 2,
+      },
+    },
+  },
+  detectRetina: true,
+  background: {
+    color: "#000000",
+    image: "",
+    position: "50% 50%",
+    repeat: "no-repeat",
+    size: "cover",
+  },
+  emitters: {
+    position: {
+      y: 55,
+      x: -30,
+    },
+    rate: {
+      delay: 7,
+      quantity: 1,
+    },
+    size: {
+      width: 0,
+      height: 0,
+    },
     particles: {
-      color: {
-        value: "#ffd966",
+      shape: {
+        type: "images",
+        options: {
+          images: [
+            {
+              src: "https://particles.js.org/images/amongus_blue.png",
+              width: 205,
+              height: 267,
+            },
+            {
+              src: "https://particles.js.org/images/amongus_cyan.png",
+              width: 207,
+              height: 265,
+            },
+            {
+              src: "https://particles.js.org/images/amongus_green.png",
+              width: 204,
+              height: 266,
+            },
+            {
+              src: "https://particles.js.org/images/amongus_lime.png",
+              width: 206,
+              height: 267,
+            },
+            {
+              src: "https://particles.js.org/images/amongus_orange.png",
+              width: 205,
+              height: 265,
+            },
+            {
+              src: "https://particles.js.org/images/amongus_pink.png",
+              width: 205,
+              height: 265,
+            },
+            {
+              src: "https://particles.js.org/images/amongus_red.png",
+              width: 204,
+              height: 267,
+            },
+            {
+              src: "https://particles.js.org/images/amongus_white.png",
+              width: 205,
+              height: 267,
+            },
+          ],
+        },
+      },
+      size: {
+        value: 40,
+      },
+      move: {
+        speed: 10,
+        outModes: {
+          default: "destroy",
+          left: "none",
+        },
+        straight: true,
+      },
+      zIndex: {
+        value: 0,
+      },
+      rotate: {
+        value: {
+          min: 0,
+          max: 360,
+        },
         animation: {
           enable: true,
           speed: 10,
+          sync: true,
         },
-      },
-      move: {
-        attract: {
-          enable: false,
-          distance: 100,
-          rotate: {
-            x: 2000,
-            y: 2000,
-          },
-        },
-        direction: "none",
-        enable: true,
-        outModes: {
-          default: "destroy",
-        },
-        path: {
-          clamp: false,
-          enable: true,
-          delay: {
-            value: 0,
-          },
-          generator: "polygonPathGenerator",
-          options: {
-            sides: 6,
-            turnSteps: 30,
-            angle: 30,
-          },
-        },
-        random: false,
-        speed: 6,
-        straight: false,
-        trail: {
-          fillColor: "#000",
-          length: 20,
-          enable: true,
-        },
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 800,
-        },
-        value: 0,
-      },
-      opacity: {
-        value: 1,
-      },
-      shape: {
-        type: "circle",
-      },
-      size: {
-        value: 2,
       },
     },
-    background: {
-      color: "#000",
-    },
-    fullScreen: {
-      zIndex: -1,
-    },
-    emitters: {
-      direction: "none",
-      rate: {
-        quantity: 1,
-        delay: 0.25,
-      },
-      size: {
-        width: 0,
-        height: 0,
-      },
-      position: {
-        x: 50,
-        y: 50,
-      },
-    },
-  });
-})();
+  },
+});
